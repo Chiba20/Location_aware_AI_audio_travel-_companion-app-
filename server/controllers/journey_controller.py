cb393686346cbb553e6d5fd25ad992a6fb285d31
Melanin08 <zda23b009@iitmz.ac.in>
2026-04-21 03:23:59 +0530

    Update journey_controller.py

---
+++ server/controllers/journey_controller.py
@@ -0,0 +1,13 @@
+import os
+from flask import jsonify, request
+from utils.file_utils import read_json, write_json
+
+BASE = os.path.dirname(os.path.dirname(__file__))
+FILE = os.path.join(BASE, "data/journeys.json")
+
+def start():
+    data = read_json(FILE)
+    body = request.json
+    data.append(body)
+    write_json(FILE, data)
+    return jsonify(body)
