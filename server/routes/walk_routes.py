6c2c6ed5e8d01762a0c342f6b4258a0a90990f39
Melanin08 <zda23b009@iitmz.ac.in>
2026-04-21 03:33:06 +0530

    Update walk_routes.py

--- server/routes/walk_routes.py
+++ server/routes/walk_routes.py
@@ -1,6 +1,6 @@
-from app import create_app
+from flask import Blueprint
+from controllers.walk_controller import get_walks
 
-app = create_app()
+bp = Blueprint("walk", __name__)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+bp.route("/")(get_walks)
