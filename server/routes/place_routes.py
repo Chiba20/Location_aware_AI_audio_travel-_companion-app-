974907cd2b0f4c76201fbe9f454c1ec262be1185
Melanin08 <zda23b009@iitmz.ac.in>
2026-04-21 03:25:15 +0530

    Update place_routes.py

--- server/routes/place_routes.py
+++ server/routes/place_routes.py
@@ -1,1 +1,8 @@
-import numpy
+from flask import Blueprint
+from controllers.place_controller import get_places, get_place, arrival
+
+bp = Blueprint("place", __name__)
+
+bp.route("/")(get_places)
+bp.route("/<int:id>")(get_place)
+bp.route("/<int:id>/arrival", methods=["POST"])(arrival)
