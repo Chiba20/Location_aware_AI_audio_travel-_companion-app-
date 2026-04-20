85371a9c28fda18a57434519e53f1faa14e68784
Melanin08 <zda23b009@iitmz.ac.in>
2026-04-21 03:24:51 +0530

    Update city_routes.py

--- server/routes/city_routes.py
+++ server/routes/city_routes.py
@@ -1,1 +1,7 @@
-import numpy
+from flask import Blueprint
+from controllers.city_controller import get_cities, get_city
+
+bp = Blueprint("city", __name__)
+
+bp.route("/")(get_cities)
+bp.route("/<int:city_id>")(get_city)
